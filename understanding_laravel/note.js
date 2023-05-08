abebf15984f1c920622b4efdbe5eec66209e2d75
Faisal ahmed <faisal2410@yahoo.com>
2023-05-08 11:34:11 +0600

    updated

---
+++ understanding_laravel/note.js
@@ -0,0 +1,62 @@
+/*
+Php artisan Commands:
+1. php artisan
+2. php artisan about
+3. php artisan make:model Post -a   or php artisan model -all
+4. php artisan inspire
+5. php artisan route:list
+
+
+*/
+
+/*
+Basic Terms :
+1.Folder Structure
+2.Artisan commands overview
+3..env file
+4. Routing
+    a. Overview and defining routes
+    b. Managing and named routes
+    c. Required Route Parameters and optional route parameters
+    d. Constraining possible Route parameters values
+5. Blade Template Engine and Views
+    a.Understanding blade template engine, views and templating
+    b. Blade inheritance and template layout
+    c. Passing and rendering data in templates
+    d. Conditional rendering
+    e. Partial Templating and loop in partial templating
+6. Request and response Lifecycle
+    a. Understanding request and response lifecycle
+    b. Headers and cookies in response code
+    c. Redirect response
+    d. Returning Json Response
+    e. Returning File Download
+    f. Grouping Routes
+    g. Request input
+7. Middleware: Running code before and after request
+
+
+
+
+*/
+
+
+/*
+Extensions :
+3. VS Code Extensions:
+PHP Extensions
+a. PHP Extension Pack
+b .PHP IntelliSense
+c. PHP Namespace Resolver
+d. phpfmt - PHP formatter
+Laravel Extensions
+a. Laravel Extension Pack
+b. Laravel Blade Formatter
+Helpful Optional
+a. Auto Close, Auto Complete, Rename Tag
+b. Prettier
+c. Rainbow Brackets
+d. Color Highlight
+
+
+*/ 
