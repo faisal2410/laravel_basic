b7490201f9f24ac24cc2396b68352a53a14fbdb6
Faisal ahmed <faisal2410@yahoo.com>
2023-06-03 17:36:06 +0600

    updated

---
+++ understanding_laravel/understanding_factory_seeder/factory.js
@@ -0,0 +1,8 @@
+/*
+Model factories are mostly used for testing purposes because it allows
+you to generate a huge amount of fake data.
+
+Seeder classes are used to insert data that is important for the system to operate with.
+
+
+*/ 
